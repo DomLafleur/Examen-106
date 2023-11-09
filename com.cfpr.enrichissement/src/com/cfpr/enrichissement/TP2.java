e97870797b2a3fb539eedfbe064f05073c767462
a0493098 <a0493098@TP-401-0037340.info.csaffluents.qc.ca>
2023-11-09 15:19:27 -0500

    Tp2

--- com.cfpr.enrichissement/src/com/cfpr/enrichissement/TP2.java
+++ com.cfpr.enrichissement/src/com/cfpr/enrichissement/TP2.java
@@ -7,6 +7,7 @@ public class TP2 {
 	public static Scanner scanner = new Scanner(System.in);
 	public static String operation;
 	public static void main(String[] args) {
+		
 		afficherMenu();       
 		operation = choixOperation(scanner, operation);
 
@@ -18,31 +19,31 @@ public class TP2 {
 
         switch (operation) {
 	        case "a", "A":
-	        	calculAddition(scanner, 0);
+	        	calculAddition();
 	            break;
 	        case "s", "S":
-	        	calculSoustraction(scanner, 0);
+	        	calculSoustraction();
 	            break;
 	        case "x", "X":
-	        	calculMultiplication(scanner, 0);
+	        	calculMultiplication();
 	            break;
 	        case "d", "D":
-	        	calculDivision(scanner, 0);
+	        	calculDivision();
 	            break;
 	        case "m", "M":
-	        	calculModulo(scanner, 0);
+	        	calculModulo();
 	            break;
 	        case "c", "C":
-	        	calculPuissanceAuCarre(scanner, 0);
+	        	calculPuissanceAuCarre();
 	            break;
 	        case "p", "P":
-	        	calculPuissance(scanner, 0);
+	        	calculPuissance();
 	            break;
 	        case "r", "R":
-	        	calculRacineCarree(scanner, 0);
+	        	calculRacineCarree();
 	            break;
 	        case "l", "L":
-	        	calculLogarithme(scanner, 0);
+	        	calculLogarithme();
 	            break;
 	        case "q", "Q":
 	            System.out.println("Vous avez quitté le programme.");
@@ -55,82 +56,158 @@ public class TP2 {
 		return operation;
     }
 	
-	public static int calculAddition(Scanner scanner, int nombre) {
-		int resultat = 0;
-		
-		nombre = scanner.nextInt();
-		
-		
-		
-		scanner.close();
-		return resultat;
-	}
-	
-	public static int calculSoustraction(Scanner scanner, int nombre) {
-		int resultat = 0;
-		
-		scanner.close();
-		return resultat;
-	}
-	
-	public static int calculMultiplication(Scanner scanner, int nombre) {
-		int resultat = 0;
-		
-		scanner.close();
-		return resultat;
-	}
-	
-	public static int calculDivision(Scanner scanner, int nombre) {
-		int resultat = 0;
-		
-		scanner.close();
-		return resultat;
-	}
-	
-	public static int calculModulo(Scanner scanner, int nombre) {
-		int resultat = 0;
-		
-		scanner.close();
-		return resultat;
-	}
-	
-	public static int calculPuissanceAuCarre(Scanner scanner, int nombre) {
-		int resultat = 0;
-		
-		scanner.close();
-		return resultat;
-	}
-	
-	public static int calculPuissance(Scanner scanner, int nombre) {
-		int resultat = 0;
-		
-		scanner.close();
-		return resultat;
-	}
-	
-	public static int calculRacineCarree(Scanner scanner, int nombre) {
-		int resultat = 0;
-		
-		scanner.close();
-		return resultat;
-	}
-	
-	public static int calculLogarithme(Scanner scanner, int nombre) {
-		int resultat = 0;
-		
-		scanner.close();
-		
-		return resultat;
-	}
-	
-	public static void fermerProgramme(Scanner scanner) {
+	public static void calculAddition() {
 		int resultat;
+		int nombre1;
+		int nombre2;
+		
+		System.out.println("Choisir le premier nombre:");
+		nombre1 = scanner.nextInt();
+		System.out.println("Choisir le deuxieme nombre:");
+		nombre2 = scanner.nextInt();
+		
+		resultat = nombre1 + nombre2;
+		System.out.println(nombre1 + " + " + nombre2 + "=" + resultat);
+		
 		
 		scanner.close();
 	}
 	
-	public static void resultatOperation() {
+	public static void calculSoustraction() {
+		int resultat;
+		int nombre1;
+		int nombre2;
 		
+		System.out.println("Choisir le premier nombre:");
+		nombre1 = scanner.nextInt();
+		System.out.println("Choisir le deuxieme nombre:");
+		nombre2 = scanner.nextInt();
+		
+		resultat = nombre1 - nombre2;
+		System.out.println(nombre1 + "-" + nombre2 + "=" + resultat);
+		
+		
+		scanner.close();
+	}
+	
+	public static void calculMultiplication() {
+		int resultat;
+		int nombre1;
+		int nombre2;
+		
+		System.out.println("Choisir le premier nombre:");
+		nombre1 = scanner.nextInt();
+		System.out.println("Choisir le deuxieme nombre:");
+		nombre2 = scanner.nextInt();
+		
+		resultat = nombre1 * nombre2;
+		System.out.println(nombre1 + "*" + nombre2 + "=" + resultat);
+		
+		
+		scanner.close();
+	}
+	
+	public static void calculDivision() {
+		int resultat;
+		int nombre1;
+		int nombre2;
+		
+		System.out.println("Choisir le premier nombre:");
+		nombre1 = scanner.nextInt();
+		System.out.println("Choisir le deuxieme nombre:");
+		nombre2 = scanner.nextInt();
+		
+		resultat = nombre1 / nombre2;
+		System.out.println(nombre1 + "/" + nombre2 + "=" + resultat);
+		
+		
+		scanner.close();
+	}
+	
+	public static void calculModulo() {
+		int resultat;
+		int nombre1;
+		int nombre2;
+		
+		System.out.println("Choisir le premier nombre:");
+		nombre1 = scanner.nextInt();
+		System.out.println("Choisir le deuxieme nombre:");
+		nombre2 = scanner.nextInt();
+		
+		resultat = nombre1 % nombre2;
+		System.out.println(nombre1 + "%" + nombre2 + "=" + resultat);
+		
+		
+		scanner.close();
+	}
+	
+	public static void calculPuissanceAuCarre() {
+		int resultat;
+		int nombre1;
+		
+		System.out.println("Choisir un nombre:");
+		nombre1 = scanner.nextInt();
+		
+		resultat = nombre1 * nombre1;
+				
+		System.out.println( nombre1+ " x2" + "=" + resultat);
+		
+		
+		scanner.close();
+	}
+	
+	public static void calculPuissance() {
+		int resultat;
+		int nombre1;
+		int nombre2;
+		
+		System.out.println("Choisir le premier nombre:");
+		nombre1 = scanner.nextInt();
+		
+		System.out.println("Choisir le deuxieme nombre:");
+		nombre2 = scanner.nextInt();
+		
+		resultat = (int) Math.pow(nombre1, nombre2);
+		
+		System.out.println(nombre1 + "Xn" + nombre2 + "=" + resultat);
+		
+		
+		scanner.close();
+	}
+	
+	public static void calculRacineCarree() {
+		int resultat;
+		int nombre1;
+		
+		System.out.println("Choisir un nombre:");
+		nombre1 = scanner.nextInt();
+		
+		resultat = (int) Math.sqrt(nombre1);
+				
+		System.out.println(nombre1 + "Rac2" + "=" + resultat);
+		
+		
+		scanner.close();
+	}
+	
+	public static void calculLogarithme() {
+		int resultat;
+		int nombre1;
+		
+		
+		System.out.println("Choisir le premier nombre:");
+		nombre1 = scanner.nextInt();
+		
+		
+		resultat = (int) Math.log(nombre1);
+		System.out.println(nombre1 + "Log" +  "=" + resultat);
+		
+		
+		scanner.close();
+	}
+	
+	public static void fermerProgramme() {
+		System.exit(0);
 	}
 	
 	private static void afficherMenu() {
